c33b3fb83c7cf51b8c8616ddabf1ae8a75d48de8
Jérémie Drouet <jeremie.drouet@gmail.com>
2019-12-28 22:47:54 +0100

    first deploy
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

---
+++ precache-manifest.bf9374eab3c41adbea862fce44b6efd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b034a2003655339a6685c7c687dad7ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f556068f8fd601d9a3c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0f2810b38138dc34049a",
+    "url": "/static/js/2.74b6251b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.74b6251b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f556068f8fd601d9a3c",
+    "url": "/static/js/main.be62a596.chunk.js"
+  },
+  {
+    "revision": "6ce50fbab82776931224",
+    "url": "/static/js/runtime-main.716abde0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
