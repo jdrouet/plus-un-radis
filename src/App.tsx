ea550336c8bd1a9147b979f335903f3c014763d5
Jérémie Drouet <jeremie.drouet@gmail.com>
2019-12-29 21:03:33 +0100

    install leaflet
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { LatLngTuple } from 'leaflet';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import './App.css';
 
 const App: React.FC = () => {
+  const position: LatLngTuple = [51.505, -0.09];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map className="App-map" center={position} zoom={13}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+      />
+    </Map>
   );
 }
 
