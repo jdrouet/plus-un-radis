43822113b21021f008568f9a3286852bd36071f0
Jérémie Drouet <jeremie.drouet@gmail.com>
2019-12-29 21:26:54 +0100

    install loader
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Loader from './component/Loader';
 import './App.css';
 
-const Map = React.lazy(() => import('./component/Map'));
+const Map = React.lazy(() =>
+  new Promise((resolve) => setTimeout(resolve, 2500))
+    .then(() => import('./component/Map')));
 
 const App: React.FC = () => {
   return (
--- src/component/Loader.tsx
+++ src/component/Loader.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    backgroundColor: theme.palette.primary.main,
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
 
 const Loader: React.FC = () => {
+  const classes = useStyles();
   return (
-    <div>Loading...</div>
+    <div className={classes.root}>
+      <CircularProgress color="secondary" />
+    </div>
   );
 }
 
